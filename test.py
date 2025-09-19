b72bd8972fee0698382fafe6a04aa648f0606b55
Manvik Talwar <manviktalwar.official@gmail.com>
2025-09-19 13:51:27 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("tempsal_data.db")
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = cursor.fetchall()
-print("Tables found:", tables)
-conn.close()
